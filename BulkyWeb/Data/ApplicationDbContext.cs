1d1c1856e0f3ade49d33a97ed0361d472cea8fc8
Prajwal R Koushik <Prajwalk@talisma.com>
2024-04-24 12:20:27 +0530

    update database connection

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "food", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "motorsports", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "art", DisplayOrder = 3 }
+                );
+        }
+
+
+    }
+
+}
