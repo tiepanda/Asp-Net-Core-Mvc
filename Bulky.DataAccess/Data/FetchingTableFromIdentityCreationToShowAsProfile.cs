a282bd4ac4a2bcc1874d28f26b6f761c33f9d799
tiepanda <prajwalrk20000@gmail.com>
2024-05-21 18:58:23 +0530

    ....

---
+++ Bulky.DataAccess/Data/FetchingTableFromIdentityCreationToShowAsProfile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Data;
+
+public partial class FetchingTableFromIdentityCreationToShowAsProfile : DbContext
+{
+    public FetchingTableFromIdentityCreationToShowAsProfile()
+    {
+    }
+
+    public FetchingTableFromIdentityCreationToShowAsProfile(DbContextOptions<FetchingTableFromIdentityCreationToShowAsProfile> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Bulky;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+            entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                .IsUnique()
+                .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+            entity.Property(e => e.Discriminator)
+                .HasMaxLength(21)
+                .HasDefaultValue("");
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.UserName).HasMaxLength(256);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Bulky.Models/AspNetUser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bulky.Models;
+
+public partial class AspNetUser
+{
+    public string Id { get; set; } = null!;
+
+    public string? UserName { get; set; }
+
+    public string? NormalizedUserName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? NormalizedEmail { get; set; }
+
+    public bool EmailConfirmed { get; set; }
+
+    public string? PasswordHash { get; set; }
+
+    public string? SecurityStamp { get; set; }
+
+    public string? ConcurrencyStamp { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public bool PhoneNumberConfirmed { get; set; }
+
+    public bool TwoFactorEnabled { get; set; }
+
+    public DateTimeOffset? LockoutEnd { get; set; }
+
+    public bool LockoutEnabled { get; set; }
+
+    public int AccessFailedCount { get; set; }
+
+    public string? City { get; set; }
+
+    public string Discriminator { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? State { get; set; }
+
+    public string? StreetAddress { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? LastName { get; set; }
+}
